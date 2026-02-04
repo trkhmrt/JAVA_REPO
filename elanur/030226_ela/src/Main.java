b0f57b7067437ad30aa5e8e20f2b4c6b624ab297
Elanur <elanureroglu59@gmail.com>
2026-02-04 19:27:47 +0300

    Bilgiler güncellendi

--- elanur/030226_ela/src/Main.java
+++ elanur/030226_ela/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         String isim = "Elanur", soyisim= "Eroglu", burc = "Ikizler";
         double saat =18.58;
         int yas = 24 ;
-
-        System.out.println("Isim : " + isim +"\nSoyisim : " + soyisim + "\nBurc : " + burc + "\nEklenme saati : " +saat + "\nYas : " +yas );
+        boolean ehliyet = yas >=18 ;
+        System.out.println("Isim : " + isim +"\nSoyisim : " + soyisim + "\nBurc : " + burc + "\nEklenme saati : " +saat + "\nYas : " +yas +"\nEhliyet alabilir mi ? : " +ehliyet);
     }
 }
