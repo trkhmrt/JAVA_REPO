ee62ff39b114f5fed3d2aaa2a6bec170bc3cd297
busepatak1-dev <busepatak1@gmail.com>
2026-02-07 17:48:06 +0300

    çalışmak için ayrı bir dosyadan ilerliyorum

---
+++ buse/Calisma dosyasi/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int yas = 18;
+    if (yas >= 18) {
+    System.out.println("Kişi 18 yaşından büyüktür.");
+} else if (yas == 17){
+        System.out.println("1 sene sonra ehliyet alabilirsiniz.");
+    }else {
+    System.out.println("Kişi ehliyet alamaz.");
+}
+                }
+
